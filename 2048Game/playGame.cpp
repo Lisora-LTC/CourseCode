7b2cb483e495b19049490b57c19ade72a0c165f9
Lisora-LTC <2650611142@qq.com>
2025-05-19 17:00:53 +0800

    完成合并逻辑

--- 2048Game/playGame.cpp
+++ 2048Game/playGame.cpp
@@ -8,13 +8,13 @@ using namespace std;
 void play_game()
 {
 	// 变量声明
-	int board[4][4] = {}; // 4*4的棋盘
+	int board[4][4] = {0}; // 4*4的棋盘
 	int score = 0;		  // 分数
 	int step = 0;		  // 步数
 	char choice = '\0';	  // 用户选择
     int exitcode=0;
 	// 游戏初始化，包括生成随机数等操作
-
+    
 	// 打印游戏界面
 	print_interface(board, score, step);
 
@@ -25,7 +25,7 @@ void play_game()
 		choice = _getch();
 		
 		// 根据用户输入进行相应操作
-        input_judge(choice,exitcode);
+        input_judge(board,choice,exitcode);
 		// 更新相关数据
 
 		// 打印游戏界面
@@ -44,11 +44,59 @@ void play_game()
 	print_exit();
 }
 
-void num_move(int type){
-
+void num_move(int board[4][4],int type){
+    // 定义起点和步进方向
+    int start_x[4] = {0, 3, 0, 0}; // 上、下、左、右
+    int start_y[4] = {0, 0, 0, 3};
+    int dx[4] = {1, -1, 0, 0};     // 行方向
+    int dy[4] = {0, 0, 1, -1};     // 列方向
+    // type: 0=up, 1=down, 2=left, 3=right
+    for(int i=0;i<4;i++){
+        int tmp[4]={0}, idx=0, last=0;
+        // 收集非零元素
+        for(int j=0;j<4;j++){
+            int x, y;
+            if(type == 0 || type == 1) { // 上或下，行变，列不变
+                x = start_x[type] + dx[type]*j;
+                y = i;
+            } else { // 左或右，列变，行不变
+                x = i;
+                y = start_y[type] + dy[type]*j;
+            }
+            if(board[x][y]!=0){
+                tmp[idx++] = board[x][y];
+            }
+        }
+        // 合并相同元素
+        int merged[4]={0}, m=0;
+        while(last<idx){
+            if(last+1<idx && tmp[last]==tmp[last+1]){
+                merged[m++] = tmp[last]*2;
+                last+=2;
+            }else{
+                merged[m++] = tmp[last++];
+            }
+        }
+        // 写回到棋盘
+        for(int j=0;j<4;j++){
+            int x, y;
+            if(type == 0 || type == 1) { // 上或下，行变，列不变
+                x = start_x[type] + dx[type]*j;
+                y = i;
+            } else { // 左或右，列变，行不变
+                x = i;
+                y = start_y[type] + dy[type]*j;
+            }
+            if (j < m) {
+                board[x][y] = merged[j];
+            } else {
+                board[x][y] = 0;
+            }
+        }
+    }
 }
 
-void input_judge(char inn, int &exitcode) {
+void input_judge(int board[4][4], char inn, int &exitcode) {
     switch (inn) {
     case 27: // ESC 键
         if (exit_confirm()) {
@@ -56,20 +104,16 @@ void input_judge(char inn, int &exitcode) {
         }
         break;
     case 72: // 上键
-        cout << "检测到上键" << endl;
-        // 添加处理上键的逻辑
+        num_move(board, 0); // 0=up
         break;
     case 80: // 下键
-        cout << "检测到下键" << endl;
-        // 添加处理下键的逻辑
+        num_move(board, 1); // 1=down
         break;
     case 75: // 左键
-        cout << "检测到左键" << endl;
-        // 添加处理左键的逻辑
+        num_move(board, 2); // 2=left
         break;
     case 77: // 右键
-        cout << "检测到右键" << endl;
-        // 添加处理右键的逻辑
+        num_move(board, 3); // 3=right
         break;
     default:
         input_fault();
