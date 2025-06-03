aa8a6cd6c4a9aa41d32ddccdc83f8cf738b1e2ec
Lisora-LTC <2650611142@qq.com>
2025-06-03 23:17:57 +0800

    重构直接修改类的类型，不使用GameState

--- PegSolitare/StateSwitch.cpp
+++ PegSolitare/StateSwitch.cpp
@@ -6,17 +6,9 @@
 
 using namespace std;
 
-// Global objects for states
 MainMenuState mainMenu;
 ChooseGameState chooseGame;
 ExitState exitState;
-
-// Global state map definition
-unordered_map<GameState, StateNode*> stateMap = {
-    {MAIN_MENU, &mainMenu},
-    {CHOOSE_GAME, &chooseGame},
-    {EXIT, &exitState}
-};
 
 // MainMenuState
 void MainMenuState::render() {
@@ -28,16 +20,18 @@ void MainMenuState::render() {
     exitButton.draw();
 }
 
-void MainMenuState::handleEvent(GameState& currentState) {
-    POINT pt;
-    GetCursorPos(&pt);
-    ScreenToClient(GetForegroundWindow(), &pt);
-
-    if (startButton.isClicked(pt.x, pt.y)) {
-        currentState = CHOOSE_GAME;
-    } else if (exitButton.isClicked(pt.x, pt.y)) {
-        currentState = EXIT;
+StateNode* MainMenuState::handleEvent() {
+    if (GetAsyncKeyState(VK_LBUTTON)) {
+        POINT pt;
+        GetCursorPos(&pt);
+        ScreenToClient(GetForegroundWindow(), &pt);
+        if (startButton.isClicked(pt.x, pt.y)) {
+            return &chooseGame;
+        } else if (exitButton.isClicked(pt.x, pt.y)) {
+            return &exitState;
+        }
     }
+    return this;
 }
 
 // ChooseGameState
@@ -49,42 +43,44 @@ void ChooseGameState::render() {
     returnButton.draw();
 }
 
-void ChooseGameState::handleEvent(GameState& currentState) {
-    POINT pt;
-    GetCursorPos(&pt);
-    ScreenToClient(GetForegroundWindow(), &pt);
-
-    if (returnButton.isClicked(pt.x, pt.y)) {
-        currentState = MAIN_MENU;
+StateNode* ChooseGameState::handleEvent() {
+    if (GetAsyncKeyState(VK_LBUTTON)) {
+        POINT pt;
+        GetCursorPos(&pt);
+        ScreenToClient(GetForegroundWindow(), &pt);
+        if (returnButton.isClicked(pt.x, pt.y)) {
+            return &mainMenu;
+        }
     }
+    return this;
 }
 
 // ExitState
 void ExitState::render() {
     // 在屏幕中央弹出一个窗口
-    setfillcolor(WHITE);
-    solidrectangle(480, 260, 800, 460); // 窗口大小
+    setfillcolor(LIGHTGRAY);
+    solidrectangle(440, 260, 840, 460); // 窗口大小
 
     settextstyle(30, 0, _T("Arial"));
     settextcolor(BLACK);
-    outtextxy(560, 300, _T("是否退出游戏？"));
+    outtextxy(1280 / 2 - textwidth(_T("是否退出游戏？")) / 2, 300, _T("是否退出游戏？"));
 
     // 绘制按钮
     yesButton.draw();
     noButton.draw();
 }
 
-void ExitState::handleEvent(GameState& currentState) {
+StateNode* ExitState::handleEvent() {
     if (GetAsyncKeyState(VK_LBUTTON)) {
         POINT pt;
         GetCursorPos(&pt);
         ScreenToClient(GetForegroundWindow(), &pt);
 
         if (yesButton.isClicked(pt.x, pt.y)) {
-            currentState = EXIT; // 退出游戏
+            return nullptr; // 结束循环退出程序
         } else if (noButton.isClicked(pt.x, pt.y)) {
-            currentState = MAIN_MENU; // 返回主菜单
-            mainMenu.render();
+            return &mainMenu; // 返回主菜单
         }
     }
-};
+    return this;
+}
